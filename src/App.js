039a64e65bda60871060daeb46a4b91ef90562f1
Khalil215 <ibrahimhamman215@gmail.com>
2022-11-03 14:47:37 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from "./components/Layout"
+import Home from "./pages/Home"
+import UseCounterCounter from "./pages/UseCounterCounter"
+import UseReducerCounter from "./pages/UseReducerCounter"
+import ErrorBoundaryCounter from "./pages/ErrorBoundaryCounter"
+import NotFound from './pages/NotFound'
+import { ErrorBoundary } from './components/ErrorBoundary'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='counter1' element={<UseCounterCounter />} />
+            <Route path='counter2' element={<UseReducerCounter />} />
+            <Route path='errorBoundary' element={<ErrorBoundary><ErrorBoundaryCounter /></ErrorBoundary>} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export default function Footer() {
+  return (
+    <Foot>
+      <div>&#169; KHALIL 2022</div>
+    </Foot>
+  )
+}
+
+const Foot = styled.footer`
+position: absolute;
+bottom: 0;
+padding: 10px;
+text-align: center;
+background-color: black;
+width: 100%;
+font-family: Ebold;
+`
